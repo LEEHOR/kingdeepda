567f77d2deb7894141b048d3c3ffa0004ddb34ca
leehor <1622293788@qq.com>
2020-10-23 11:10:59 +0800

    添加工序汇报

---
+++ app/src/main/java/com/jeewms/www/wms/ui/activity/processReport/ProcessReportDetail.java
@@ -0,0 +1,36 @@
+package com.jeewms.www.wms.ui.activity.processReport;
+
+import android.os.Bundle;
+
+import com.jeewms.www.wms.R;
+import com.jeewms.www.wms.base.BaseActivity1;
+
+/**
+ * @ProjectName: kingdeepda
+ * @Package: com.jeewms.www.wms.ui.activity.processReport
+ * @ClassName: ProcessReportDetail
+ * @Description: java类作用描述
+ * 工序汇报详情
+ * @Author: 作者名
+ * @CreateDate: 2020/10/23 9:06
+ * @UpdateUser: 更新者：
+ * @UpdateDate: 2020/10/23 9:06
+ * @UpdateRemark: 更新说明：
+ * @Version: 1.0
+ */
+public class ProcessReportDetail extends BaseActivity1 {
+    @Override
+    protected int getContentResId() {
+        return R.layout.activity_process_report_detail;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initfun() {
+
+    }
+}
